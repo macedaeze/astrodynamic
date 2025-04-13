9e8b5bbf9d92742c4adcda053e5b96eafdeb40d4
macedaeze <ezemaceda@gmail.com>
2025-04-13 13:31:11 -0300

    minor changes and exercise 7 solved

--- Guia 2/Ej2_7.py
+++ Guia 2/Ej2_7.py
@@ -1,7 +1,10 @@
+from Ej2_2 import getMeanAnomaly
 import math
 import unittest
 
-def getTrueAnomaly(eccentricity, eccentricAnomaly, semilactusRectum = None, rDistance = None):
+# Itero la funcion del 2_6 para que sea mas generica y calcule la anomalia verdadera en base a distintos casos
+
+def getTrueAnomaly(eccentricity, eccentricAnomaly = None, meanAnomaly = None, semilactusRectum = None, rDistance = None):
     """
     Funcion que calcula la anomalia verdadera a partir de la excentricidad y la anomalia excentrica.
     
@@ -13,6 +16,9 @@ def getTrueAnomaly(eccentricity, eccentricAnomaly, semilactusRectum = None, rDis
     float: Anomalia verdadera [en radianes]
     """
     
+    if eccentricAnomaly == None:
+        eccentricAnomaly = getMeanAnomaly(eccentricity, meanAnomaly)
+
     # Para el caso eliptico
     if eccentricity < 1.0:
         trueAnomaly = math.asin(math.sin(eccentricAnomaly) * math.sqrt(1 - eccentricity**2) / (1 + eccentricity * math.cos(eccentricAnomaly) ) )
@@ -29,22 +35,26 @@ def getTrueAnomaly(eccentricity, eccentricAnomaly, semilactusRectum = None, rDis
 
 
 #TEST UNITARIOS
+
 class TestGetTrueAnomaly(unittest.TestCase):
 
+    """
+    Clase de test para verificar la función getTrueAnomaly.
+    """
     def test_matrix_of_cases(self):
         """
         Matriz de casos sencillos para verificar la función getTrueAnomaly.
         """
         test_cases = [
-            # ( e,  eccentricAnom,  expected )
+            # ( e,  meanAnomaly,       expected )
             (0.0,   0.0,            0.0),  
-            (0.5,   1.0472,         0.6435),  
-            (1.5,   1.0472,         1.6424), 
+            (0.5,   1.0472,         0.36217),  
+            (1.5,   1.0472,         0.54617), 
         ]
 
-        for eccentricity, eccentricAnomaly, expected in test_cases:
-            with self.subTest(eccentricity=eccentricity, eccentricAnomaly=eccentricAnomaly):
-                result = getTrueAnomaly(eccentricity, eccentricAnomaly)
+        for eccentricity, meanAnomaly, expected in test_cases:
+            with self.subTest(eccentricity=eccentricity, trueAnomaly=meanAnomaly):
+                result = getTrueAnomaly(eccentricity, meanAnomaly=meanAnomaly)
                 self.assertAlmostEqual(result, expected, places=3)
 
     def test_parabolicCase(self):
@@ -52,14 +62,12 @@ class TestGetTrueAnomaly(unittest.TestCase):
         Caso parabolico para verificar la función getTrueAnomaly.
         """
         eccentricity = 1.0
-        eccentricAnomaly = 1.0472
+        meanAnomaly = 1.0472
         semilactusRectum = 1000000.0
         rDistance = 2000000.0
-        expected = 0.55107
-        result = getTrueAnomaly(eccentricity, eccentricAnomaly, semilactusRectum, rDistance)
+        expected = 0.0907
+        result = getTrueAnomaly(eccentricity, meanAnomaly=meanAnomaly, semilactusRectum = semilactusRectum, rDistance = rDistance)
         self.assertAlmostEqual(result, expected, places=3)
 
-
 if __name__ == "__main__":
-    
     unittest.main()
